6f8b77ab6dec5ddb06ebda03be4f1393aacc6cac
rizkynandaanggia <127828416+rizkynandaanggia@users.noreply.github.com>
2023-04-03 20:52:28 +0700

    Add files via upload

---
+++ Mobil.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TugasPertemuan4
+{
+    public class Mobil
+    {
+        // private fields
+        private string warna_Mobil;
+        private int jumlahPintu_Mobil;
+        private string merek_Mobil;
+        private string model_Mobil;
+        private int tahunKeluaran_Mobil;
+
+        // public properties
+        public string Warna_Mobil
+        {
+            get { return warna_Mobil; }
+            set { warna_Mobil = value; }
+        }
+
+        public int JumlahPintu_Mobil
+        {
+            get { return jumlahPintu_Mobil; }
+            set { jumlahPintu_Mobil = value; }
+        }
+
+        public string Merek_Mobil
+        {
+            get { return merek_Mobil; }
+            set { merek_Mobil = value; }
+        }
+
+        public string Model_Mobil
+        {
+            get { return model_Mobil; }
+            set { model_Mobil = value; }
+        }
+
+        public int TahunKeluaran_Mobil
+        {
+            get { return tahunKeluaran_Mobil; }
+            set { tahunKeluaran_Mobil = value; }
+        }
+
+        // public methods
+        public void InfoMobil()
+        {
+            Console.WriteLine("Warna          : {0}", Warna_Mobil);
+            Console.WriteLine("Jumlah Pintu   : {0}", JumlahPintu_Mobil);
+            Console.WriteLine("Merek          : {0}", Merek_Mobil);
+            Console.WriteLine("Model          : {0}", Model_Mobil);
+            Console.WriteLine("Tahun Keluaran : {0}", TahunKeluaran_Mobil);
+        }
+
+        public void Gas(int Kecepatan)
+        {
+            Console.WriteLine("\nMobil " + Model_Mobil + " berjalan dengan kecepatan " + Kecepatan + "\n");
+        }
+
+        public void Klakson(string Suara)
+        {
+            Console.WriteLine("{0}\n", Suara);
+        }
+
+        public void TampilanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna " + Warna_Mobil + ", merek " + Merek_Mobil + ", model " + Model_Mobil + " keluaran tahun " + TahunKeluaran_Mobil + " dengan jumlah pintu sebanyak " + JumlahPintu_Mobil + "");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace TugasPertemuan4
+{
+    internal class Program
+    {
+        class program
+        {
+
+            static void Main(string[] args)
+            {
+                Mobil mobil1 = new Mobil();
+                mobil1.Warna_Mobil = "Silver";
+                mobil1.JumlahPintu_Mobil = 4;
+                mobil1.Merek_Mobil = "Toyota";
+                mobil1.Model_Mobil = "Rush TRD Sportivo";
+                mobil1.TahunKeluaran_Mobil = 2016;
+
+                mobil1.InfoMobil();
+                mobil1.Gas(80); // Output: Mobil Rush TRD Sportivo berjalan dengan kecepatan 80
+                mobil1.Klakson("Telolet"); // Output: Telolet
+                mobil1.TampilanInfo(); // Output: Mobil saya berwarna Silver, merek Toyota model Rush TRD Sportivo keluaran tahun 2016 dengan jumlah pintu sebanyak 4
+            }
+        }
+
+    }
+}
